50152977baab7035d85207aca6fb8a415f809777
Electrolyte Orb <electrolyte.orb@gmail.com>
2021-03-12 17:28:22 +0530

    Added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'system-ui'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
