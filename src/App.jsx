e995bad22935c75c004f8c51765c50800a6eb406
Electrolyte Orb <electrolyte.orb@gmail.com>
2021-03-13 19:09:11 +0530

    added framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import { motion } from 'framer-motion';
 const App = () => {
   return (
-    <div className="h-screen bg-black text-white max-h-screen">
+    <div className="min-h-screen bg-black text-white">
       <Navbar></Navbar>
-      <div className="px-6 py-4">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="px-6 py-4"
+      >
         <div>Verbose 9's</div>
         <h1 className="text-6xl font-medium leading-tight my-4">Project U7</h1>
         <p className="text-sm italic text-gray-400">Everybody's welcome</p>
-      </div>
+        <div className="mt-6">
+          <motion.button
+            whileTap={{ scale: 0.9 }}
+            className="px-6 py-2 rounded-full bg-blue-600"
+          >
+            Let me in
+          </motion.button>
+
+          <div className="h-screen"></div>
+        </div>
+      </motion.div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Burger from '@animated-burgers/burger-arrow';
 import '@animated-burgers/burger-arrow/dist/styles.css';
+import { AnimatePresence, motion } from 'framer-motion';
 const Navbar = () => {
   const [burger, setBurger] = useState(false);
+  useEffect(() => {
+    document.body.addEventListener('scroll', (e) => {
+      console.log(e);
+    });
+  }, []);
   return (
-    <div className="sticky w-full border-b border-gray-500 p-6 flex justify-between items-center">
-      <div className="font-semibold">Verbose 9</div>
-      <Burger
-        style={{ fontSize: '10px' }}
-        isOpen={burger}
-        direction="up"
-        onClick={() => setBurger(!burger)}
-      />
-      {burger && (
-        <div
-          className="fixed left-0 w-full bg-black shadow-md border-b-2 border-gray-500 p-6"
-          style={{ top: 79 }}
-        >
-          <div className="font-semibold text-lg">This is Menu</div>
-          <ul className="select-none mt-4 text-gray-300">
-            <li>Home</li>
-          </ul>
-        </div>
-      )}
+    <div className="w-full sticky top-0 z-40">
+      <div className="bg-black border-b border-gray-500 p-6 items-center justify-between flex z-40">
+        <div className="font-semibold">Verbose 9</div>
+        <Burger
+          style={{
+            fontSize: '10px',
+          }}
+          isOpen={burger}
+          direction="up"
+          onClick={() => setBurger(!burger)}
+        />
+      </div>
+
+      <AnimatePresence>
+        {burger && (
+          <motion.div
+            initial={{ y: '-100%' }}
+            animate={{ y: 0 }}
+            exit={{ y: '-110%' }}
+            transition={{ type: 'tween', duration: 0.35 }}
+            className="fixed left-0 w-full bg-black shadow-md border-b border-gray-500 p-6"
+            style={{ top: 79, zIndex: '-1' }}
+          >
+            <div className="font-semibold text-lg">This is Menu</div>
+            <ul className="select-none mt-4 text-gray-300">
+              <li>Home</li>
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
