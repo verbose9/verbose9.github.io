912cab5560dc00244375bd8a585913530e736ad0
Electrolyte Orb <electrolyte.orb@gmail.com>
2021-03-13 16:14:30 +0530

    edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div className="m-4">
       <div className="font-medium text-8xl my-8 text-center">Project U7</div>
+      <div>master B</div>
       <button className="py-4 px-8 bg-blue-500 rounded-xl text-white">
         Press me!
       </button>
