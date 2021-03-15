eda57453a9769742f3d7834d4092d4e2e75e0751
Electrolyte Orb <electrolyte.orb@gmail.com>
2021-03-15 20:23:31 +0530

    improved design

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,49 @@ const App = () => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
-        className="px-6 py-4"
+        className="px-6 py-4 text-lg"
       >
-        <div>Verbose 9's</div>
-        <h1 className="text-6xl font-medium leading-tight my-4">Project U7</h1>
-        <p className="text-sm italic text-gray-400">Everybody's welcome</p>
+        <h1 className="text-6xl font-medium leading-tight my-4">
+          Project <br />
+          U7
+        </h1>
+        <p className="text-2xl mt-6 text-gray-400 leading-relaxed">
+          A collaborative project
+          <br />
+          where developers
+          <br />
+          collaborate to the
+          <br />
+          Verbose 9 OS Project.
+          <br />
+          You are welcomed with
+          <br />
+          our whole heart.
+          <br />
+        </p>
         <div className="mt-6">
-          <motion.button
-            whileTap={{ scale: 0.9 }}
-            className="px-6 py-2 rounded-full bg-blue-600"
-          >
-            Let me in
-          </motion.button>
-
-          <div className="h-screen"></div>
+          <button className="px-6 py-2 rounded-none border-2 border-white text-white collab-btn text-xl">
+            Collab
+          </button>
+        </div>
+        <div className="mt-20 py-10 tech-stack z-20 relative overflow-hidden max-w-full">
+          <div className="z-50">
+            <h1 className="font-bold text-5xl text-white">
+              Our Tech
+              <br />
+              Stack
+            </h1>
+            <ul className="mt-8 space-y-4 text-2xl">
+              <li>React</li>
+              <li>MonogoDB</li>
+              <li>Node.js</li>
+              <li>Express</li>
+              <li>#DevOps</li>
+            </ul>
+          </div>
+        </div>
+        <div className="mt-20">
+          <img src="/circuit.svg" className="w-full" />
         </div>
       </motion.div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Burger from '@animated-burgers/burger-arrow';
 import '@animated-burgers/burger-arrow/dist/styles.css';
 import { AnimatePresence, motion } from 'framer-motion';
+
 const Navbar = () => {
   const [burger, setBurger] = useState(false);
-  useEffect(() => {
-    document.body.addEventListener('scroll', (e) => {
-      console.log(e);
-    });
-  }, []);
   return (
     <div className="w-full sticky top-0 z-40">
-      <div className="bg-black border-b border-gray-500 p-6 items-center justify-between flex z-40">
-        <div className="font-semibold">Verbose 9</div>
+      <div className="bg-black p-6 items-center justify-between flex z-40">
+        <div className="flex items-center">
+          <img src="/logo.svg" className="h-7" />
+        </div>
         <Burger
           style={{
             fontSize: '10px',
@@ -28,14 +26,24 @@ const Navbar = () => {
           <motion.div
             initial={{ y: '-100%' }}
             animate={{ y: 0 }}
-            exit={{ y: '-110%' }}
+            exit={{ y: '-100%' }}
             transition={{ type: 'tween', duration: 0.35 }}
-            className="fixed left-0 w-full bg-black shadow-md border-b border-gray-500 p-6"
-            style={{ top: 79, zIndex: '-1' }}
+            className="fixed left-0 w-full bg-white text-black shadow-m px-6 py-10 border-b border-black"
+            style={{ top: 78, zIndex: '-1' }}
           >
-            <div className="font-semibold text-lg">This is Menu</div>
-            <ul className="select-none mt-4 text-gray-300">
-              <li>Home</li>
+            <ul className="select-none space-y-6 text-xl">
+              <li>
+                <span className="nav-link">Home</span>
+              </li>
+              <li>
+                <span className="nav-link">Contact us</span>
+              </li>
+              <li>
+                <span className="nav-link">Projects</span>
+              </li>
+              <li>
+                <span className="nav-link">Github</span>
+              </li>
             </ul>
           </motion.div>
         )}
